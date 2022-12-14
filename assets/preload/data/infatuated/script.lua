b9b6aa94320e085673dbf7856a5cfbf2a06d584d
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-12-13 21:16:22 -0300

    Add files via upload

---
+++ assets/preload/data/infatuated/script.lua
@@ -0,0 +1,230 @@
+
+
+function onCreate()
+    function onStepHit()
+     
+    if curStep == 178 then
+            
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            addLuaSprite('bartop', false);
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            addLuaSprite('barbot', false);
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end
+    if curStep == 192 then
+            makeLuaSprite('Blue', 'Blue', -400, -300);
+            setLuaSpriteScrollFactor('Blue', 0, 0);
+            scaleObject('Blue', 1.1, 1.1);
+            addLuaSprite('Blue', true);    
+        end
+    if curStep == 320 then 
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+            
+        end
+
+    if curStep == 448 then
+            
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            addLuaSprite('bartop', false);
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            addLuaSprite('barbot', false);
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end
+
+    if curStep == 512 then 
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+            
+        end
+
+     if curStep == 560 then
+            
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            addLuaSprite('bartop', false);
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            addLuaSprite('barbot', false);
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end
+
+        if curStep == 576 then 
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end
+
+        if curStep == 704 then
+            
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            addLuaSprite('bartop', false);
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            addLuaSprite('barbot', false);
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end
+
+        if curStep == 736 then 
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end
+
+        if curStep == 768 then
+            
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            addLuaSprite('bartop', false);
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            addLuaSprite('barbot', false);
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end
+
+        if curStep == 816 then 
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end
+
+        if curStep == 1088 then
+            
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            addLuaSprite('bartop', false);
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            addLuaSprite('barbot', false);
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end
+
+        if curStep == 1216 then 
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end
+
+        if curStep == 1376 then
+            
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            addLuaSprite('bartop', false);
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            addLuaSprite('barbot', false);
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end
+
+        if curStep == 1408 then 
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end
+
+        if curStep == 1424 then
+            
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            addLuaSprite('bartop', false);
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            addLuaSprite('barbot', false);
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end
+
+        if curStep == 1440 then 
+            makeLuaSprite('bartop','',0, -30);
+            makeGraphic('bartop',1280,100,'000000');
+            setObjectCamera('bartop','hud');
+            setScrollFactor('bartop',0,0);
+
+            makeLuaSprite('barbot','',0,650);
+            makeGraphic('barbot',1280,100,'000000');
+            setScrollFactor('barbot',0,0);
+            setObjectCamera('barbot','hud');
+        end                                                                                                                                                                          
+    end
+end
+
